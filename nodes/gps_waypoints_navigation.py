b0c1da48706b342cf9c3765ca0c9e03addfadfce
rasheeddo <rasheedo.kit@gmail.com>
2021-10-25 20:45:38 +0900

    added argparser for yaml and mission files

--- nodes/gps_waypoints_navigation.py
+++ nodes/gps_waypoints_navigation.py
@@ -6,16 +6,18 @@ import rosparam
 import yaml
 import dynamic_reconfigure.client
 import os
+import argparse
 
 import time
-from std_msgs.msg import Float32MultiArray, Int32MultiArray, Bool, Int16, String, Int8
+from std_msgs.msg import Float32MultiArray, Int32MultiArray, Bool, Int16, String, Int8, UInt16
+from jmoab_autopilot_ros.msg import GoalWaypoints
 from sensor_msgs.msg import NavSatFix
 import numpy as np
 from simple_pid import PID
 
-class GPS_NAV:
+class GPS_NAV(object):
 
-	def __init__(self):
+	def __init__(self, param_file, mission_file):
 
 		################################ Init/Pub/Sub #####################################
 		rospy.init_node("gps_waypoints_autopilot_node", anonymous=True)
@@ -28,16 +30,24 @@ class GPS_NAV:
 		self.sbus_cmd_pub = rospy.Publisher("/sbus_cmd", Int32MultiArray, queue_size=10)
 		self.sbus_cmd = Int32MultiArray()
 
+		rospy.Subscriber("/heartbeat", Bool, self.heartbeat_callback)
+		rospy.Subscriber("/goal_waypoints", GoalWaypoints, self.goal_wp_callback)
+		rospy.Subscriber("/request_store_waypoints", Bool, self.request_wp_callback)
+
+		self.reply_wp_pub = rospy.Publisher("/reply_goal_waypoints", GoalWaypoints, queue_size=10)
+		self.reply_wp_msg = GoalWaypoints()
+		self.arrived_wp_pub = rospy.Publisher("/arrived_waypoints", UInt16, queue_size=10)
+		self.arrived_wp_msg = UInt16()
+
+		self.atcart_mode_cmd_pub = rospy.Publisher("/atcart_mode_cmd", Int8, queue_size=10)
+		self.atcart_mode_cmd_msg = Int8()
+
 		################################ ROS Parameters #####################################
 		sv_node = "gps_waypoints_server_node"
-		rospack = rospkg.RosPack()
-		jmoab_autopilot_ros_path = rospack.get_path("jmoab_autopilot_ros")
-		yaml_name = "GpsWaypoints.yaml"
-		yaml_path = os.path.join(jmoab_autopilot_ros_path, "cfg", yaml_name)
 
 		# https://answers.ros.org/question/169866/load-yaml-with-code/
 		# load yaml file to rosparam server without running server on python
-		f = open(yaml_path, 'r')
+		f = open(param_file, 'r')
 		yamlfile = yaml.load(f)
 		rosparam.upload_params("/", yamlfile)
 
@@ -63,6 +73,7 @@ class GPS_NAV:
 		self.cross_p = rosparam.get_param(sv_node+"/cross_p")
 		self.cross_i = rosparam.get_param(sv_node+"/cross_i")
 		self.cross_d = rosparam.get_param(sv_node+"/cross_d")
+		self.use_heartbeat = rosparam.get_param(sv_node+"/use_heartbeat")
 
 		########################### GPS Parameters ##########################  
 		self.lat = 0.0
@@ -70,11 +81,22 @@ class GPS_NAV:
 		self.hdg = 0.0
 
 		mission_name = "mission.txt"
-		self.mission_path = os.path.join(jmoab_autopilot_ros_path, "waypoints", mission_name)
+		# self.mission_path = os.path.join(jmoab_autopilot_ros_path, "waypoints", mission_name)
+		self.mission_path = mission_file
 
 		self.lat_target_list = []
 		self.lon_target_list = []
+		self.speed_target_list = []
+		self.delay_target_list = []
+		self.sbus_throttle_target_list = []
 		self.total_points = 0
+		self.target_wp = 0
+
+		self.got_new_wps = False
+
+		self.hb_timestamp = time.time()
+		self.hb_timeout = 10.0
+		self.hb_period = 0.0
 
 		########################### PID Parameters ##########################
 		## PID heading steering control
@@ -125,8 +147,11 @@ class GPS_NAV:
 		#self.sbus_steering_mid = 969	#1024
 		self.cart_mode = 0
 		self.completed_flag = False
-
 		self.hdg_calib_flag = False
+
+		self.sbus_max = 1680
+		self.vel_min = 0.2
+		self.vel_max = 2.2
 
 		self.update_local_param()
 
@@ -138,23 +163,81 @@ class GPS_NAV:
 		rospy.spin()
 
 	def get_target_points(self):
-		print("Extract lat/lon from mission.txt")
-		file = open(self.mission_path, 'r')
-		lines = file.read().splitlines()
 
-		for line in lines:
-			if len(line)<40:
-				pass
-			else:
-				line_list = line.split('\t')
-				self.lat_target_list.append(float(line_list[8]))
-				self.lon_target_list.append(float(line_list[9]))
+		## Reset all previos data
+		self.lat_target_list = []
+		self.lon_target_list = []
+		self.speed_target_list = []
+		self.delay_target_list = []
+		self.sbus_throttle_target_list = []
 
-		self.total_points = len(self.lat_target_list)
+		if os.path.exists(self.mission_path):
+			print("Extract lat/lon from mission.txt")
+			file = open(self.mission_path, 'r')
+			lines = file.read().splitlines()
 
-		print("lat_target_list", self.lat_target_list)
-		print("lon_target_list", self.lon_target_list)
+			for line in lines:
+				if len(line)<40:
+					pass
+				else:
+					line_list = line.split('\t')
+					self.speed_target_list.append(float(line_list[4]))
+					self.delay_target_list.append(float(line_list[5]))
+					self.lat_target_list.append(float(line_list[8]))
+					self.lon_target_list.append(float(line_list[9]))
 
+			file.close()
+
+			self.total_points = len(self.lat_target_list)
+
+			for speed in self.speed_target_list:
+				self.sbus_throttle_target_list.append(self.vel2sbus(speed))
+
+			print("lat_target_list", self.lat_target_list)
+			print("lon_target_list", self.lon_target_list)
+			print("speed_target_list", self.speed_target_list)
+			print("delay_target_list", self.delay_target_list)
+			print("sbus_throttle_target_list", self.sbus_throttle_target_list)
+
+	def generate_mission_file(self, current_wp, coord_frame, command, param1, param2, param3, param4, lat_list, lon_list, alt_list, autocontinue):
+		
+		mission_file = open(self.mission_path, 'w+')
+		mission_file.write("QGC WPL 110\n")
+
+		for i in range(len(lat_list)):
+			mission_file.write('{:d}'.format(i))
+			mission_file.write("\t")
+			mission_file.write("{:d}".format(current_wp[i]))
+			mission_file.write("\t")
+			mission_file.write("{:d}".format(coord_frame[i]))
+			mission_file.write("\t")
+			mission_file.write("{:d}".format(command[i]))
+			mission_file.write("\t")
+			mission_file.write("{:.6f}".format(param1[i]))
+			mission_file.write("\t")
+			mission_file.write("{:.6f}".format(param2[i]))
+			mission_file.write("\t")
+			mission_file.write("{:.6f}".format(param3[i]))
+			mission_file.write("\t")
+			mission_file.write("{:.6f}".format(param4[i]))
+			mission_file.write("\t")
+			mission_file.write('{:.16f}'.format(lat_list[i]))
+			mission_file.write("\t")
+			mission_file.write('{:.16f}'.format(lon_list[i]))
+			mission_file.write("\t")
+			mission_file.write("{:.2f}".format(alt_list[i]))
+			mission_file.write("\t")
+			mission_file.write("{:d}".format(autocontinue[i]))
+			if i != (len(lat_list)-1):
+				mission_file.write("\n")
+
+		mission_file.close()
+
+	def heartbeat_callback(self, msg):
+
+		if msg.data == True:
+
+			self.hb_timestamp = time.time()
 
 	def param_callback(self, config):
 		# rospy.loginfo("Config set to {int_param}, {double_param}, {str_param}, {bool_param}".format(**config))
@@ -180,6 +263,7 @@ class GPS_NAV:
 		self.cross_p = config["cross_p"]
 		self.cross_i = config["cross_i"]
 		self.cross_d = config["cross_d"]
+		self.use_heartbeat = config["use_heartbeat"]
 		self.pid_hdg.tunings = (self.hdg_p, self.hdg_i, self.hdg_d)
 		self.pid_x.tunings = (self.cross_p, self.cross_i, self.cross_d)
 
@@ -198,6 +282,40 @@ class GPS_NAV:
 		# Cross track steering	
 		self.sbus_steering_max = self.max_start_str + self.str_adj  # steer to right
 		self.sbus_steering_min = self.min_start_str - self.str_adj  # steer to left
+
+	def goal_wp_callback(self, msg):
+		print("New goal_waypoints come")
+		self.total_wps = len(msg.lat.data)
+		current_wp_list = [0]*self.total_wps
+		coord_frame_list = [3]*self.total_wps
+		command_list = [16]*self.total_wps
+		param1_list = msg.speed.data	# speed list
+		param2_list = msg.delay.data	# delay list
+		param3_list = [0]*self.total_wps
+		param4_list = [0]*self.total_wps
+		lat_list = msg.lat.data
+		lon_list = msg.lon.data
+		alt_list = [10]*self.total_wps
+		autocon_list = [1]*self.total_wps
+
+		self.got_new_wps = True
+
+		self.generate_mission_file(current_wp_list, coord_frame_list, command_list, \
+			param1_list, param2_list, param3_list, param4_list, \
+			lat_list, lon_list, alt_list, autocon_list)
+
+		self.reply_wp_pub.publish(msg)
+
+	def request_wp_callback(self,msg):
+
+		print("Request waypoints")
+		if msg.data == True:
+			self.reply_wp_msg.lat.data = self.lat_target_list
+			self.reply_wp_msg.lon.data = self.lon_target_list
+			self.reply_wp_msg.speed.data = self.speed_target_list
+			self.reply_wp_msg.delay.data = self.delay_target_list
+
+			self.reply_wp_pub.publish(self.reply_wp_msg)
 
 	def gps_callback(self, msg):
 
@@ -219,6 +337,22 @@ class GPS_NAV:
 		# 	self.completed_flag = False
 	def hdg_calib_flag_callback(self, msg):
 		self.hdg_calib_flag = msg.data
+
+		if msg.data == True:
+			print("=============== got calib flag =================")
+
+	def vel2sbus(self, vel):
+
+		if vel < 0.05:
+			sbus = 1024
+		elif 0.05 <= vel < self.vel_min:
+			sbus = self.thr_slowest
+		elif (self.vel_min <= vel <= self.vel_max):
+			sbus = self.map_with_limit(vel, self.vel_min, self.vel_max, self.thr_slowest, self.sbus_max)
+		elif vel > self.vel_max:
+			sbus = self.sbus_max
+
+		return int(sbus)
 
 	def get_distance(self, lat1, lon1, lat2, lon2):
 
@@ -329,9 +463,9 @@ class GPS_NAV:
 
 	def loop(self):
 
-		rate = rospy.Rate(100)
+		rate = rospy.Rate(30)
 		print("Start GPS Waypoints Navigation")
-		target_wp = 0
+
 
 		## it must enter get_goal_ang_once first
 		get_goal_ang_once = True
@@ -354,8 +488,10 @@ class GPS_NAV:
 		## for checking cart mode
 		cart_mode_check_lock = False
 		from_step = 0
-
 		pause_flag = False
+
+		## for heartbeat
+		hb_trig_once = True
 
 		while not rospy.is_shutdown():
 			
@@ -396,7 +532,7 @@ class GPS_NAV:
 					##### only one time               #####
 					#######################################
 					if get_goal_ang_once:
-						goal_ang = self.get_bearing(self.lat, self.lon, self.lat_target_list[target_wp], self.lon_target_list[target_wp]) 
+						goal_ang = self.get_bearing(self.lat, self.lon, self.lat_target_list[self.target_wp], self.lon_target_list[self.target_wp]) 
 						## goal_ang is in 180ranges, hdg is in 360ranges
 						diff_ang, sign = self.find_smallest_diff_ang(goal_ang, self.hdg)
 						goal_hdg = self.hdg + (diff_ang*sign)
@@ -439,7 +575,7 @@ class GPS_NAV:
 					##### only one time                      #####
 					##############################################
 					if get_goal_dist_once:
-						goal_dist = self.get_distance(self.lat, self.lon, self.lat_target_list[target_wp], self.lon_target_list[target_wp])
+						goal_dist = self.get_distance(self.lat, self.lon, self.lat_target_list[self.target_wp], self.lon_target_list[self.target_wp])
 						start_lat = self.lat
 						start_lon = self.lon
 						cur_dist = 0.0
@@ -460,20 +596,20 @@ class GPS_NAV:
 
 						## A is point of previous point 
 						## in case of just start there is no -1 element in target_list so we use previous lat/lon
-						if (target_wp == 0):
+						if (self.target_wp == 0):
 							A_lat = start_lat
 							A_lon = start_lon
 						else:
-							A_lat = self.lat_target_list[target_wp-1]
-							A_lon = self.lon_target_list[target_wp-1]
+							A_lat = self.lat_target_list[self.target_wp-1]
+							A_lon = self.lon_target_list[self.target_wp-1]
 
 						## reset_flag tells if the cart went out of the route, so it will re-target heading with current pose 
-						## if no reset_flag, it will just calculate angle to goal/bot with target_wp-1 and target_wp
+						## if no reset_flag, it will just calculate angle to goal/bot with self.target_wp-1 and self.target_wp
 						if reset_flag == True:
-							ang_A_to_goal = self.get_bearing(start_lat, start_lon, self.lat_target_list[target_wp], self.lon_target_list[target_wp])
+							ang_A_to_goal = self.get_bearing(start_lat, start_lon, self.lat_target_list[self.target_wp], self.lon_target_list[self.target_wp])
 							ang_A_to_bot = self.get_bearing(start_lat, start_lon, self.lat, self.lon)
 						else:
-							ang_A_to_goal = self.get_bearing(A_lat, A_lon, self.lat_target_list[target_wp], self.lon_target_list[target_wp])
+							ang_A_to_goal = self.get_bearing(A_lat, A_lon, self.lat_target_list[self.target_wp], self.lon_target_list[self.target_wp])
 							ang_A_to_bot = self.get_bearing(A_lat, A_lon, self.lat, self.lon)
 
 
@@ -497,15 +633,15 @@ class GPS_NAV:
 							# previous point and current pose
 							a = self.get_distance(start_lat, start_lon, self.lat, self.lon)
 							# previous point and goal point
-							c = self.get_distance(start_lat, start_lon, self.lat_target_list[target_wp], self.lon_target_list[target_wp])
+							c = self.get_distance(start_lat, start_lon, self.lat_target_list[self.target_wp], self.lon_target_list[self.target_wp])
 						else:
 							# previous point and current pose
 							a = self.get_distance(A_lat, A_lon, self.lat, self.lon)
 							# previous point and goal point
-							c = self.get_distance(A_lat, A_lon, self.lat_target_list[target_wp], self.lon_target_list[target_wp])
+							c = self.get_distance(A_lat, A_lon, self.lat_target_list[self.target_wp], self.lon_target_list[self.target_wp])
 
 						# current pose and goal point
-						b = self.get_distance(self.lat, self.lon, self.lat_target_list[target_wp], self.lon_target_list[target_wp])
+						b = self.get_distance(self.lat, self.lon, self.lat_target_list[self.target_wp], self.lon_target_list[self.target_wp])
 					
 						k = (a**2 - b**2 + c**2)/(2*c)
 						x = np.sqrt(a**2 - k**2)
@@ -588,10 +724,15 @@ class GPS_NAV:
 						## if very close distance, use very low speed, ignore user speed command ##
 						###########################################################################
 						if goal_dist < 2.0:
-							sbus_throttle = self.thr_slowest
+							sbus_throttle = self.thr_slowest+20
 						else:
-							sbus_throttle = self.sbus_throttle_const
-
+							if abs(goal_dist - cur_dist) < 2.0:
+								sbus_throttle = self.thr_slowest+20
+							else:
+								if self.speed_target_list[self.target_wp] != 0.0:
+									sbus_throttle = self.sbus_throttle_target_list[self.target_wp] #self.sbus_throttle_const
+								else:
+									sbus_throttle = self.sbus_throttle_const
 
 						###############################################################
 						### Resest target heading again if cross track error is big ###
@@ -612,7 +753,7 @@ class GPS_NAV:
 						### Print out all necessary info ###
 						####################################
 						print("PID: {:} | tg_wp: {:d} | x: {:.2f} | rst_T/O: {:.2f} | rst_flg: {:} | AngAtoBot: {:.2f} | hdg: {:.2f} | AngAtoGoal: {:.2f} | hdgDff: {:.2f} | outPID: {:.2f} | goalDist: {:.2f} curDist: {:.2f} | str: {:d} | thr: {:d}".format(\
-							pid_mode, target_wp, x_track_error, reset_timeout, reset_flag, ang_A_to_bot, self.hdg, ang_A_to_goal, hdg_diff, OUTPUT_PID, goal_dist, cur_dist, sbus_steering, sbus_throttle))
+							pid_mode, self.target_wp, x_track_error, reset_timeout, reset_flag, ang_A_to_bot, self.hdg, ang_A_to_goal, hdg_diff, OUTPUT_PID, goal_dist, cur_dist, sbus_steering, sbus_throttle))
 
 
 						## update cur_dist with current lat/lon compare to start lat/lon
@@ -635,14 +776,17 @@ class GPS_NAV:
 					if (throttle_flag == False) and (turning_flag == False) and (end_flag == True):
 						sbus_steering = self.sbus_steering_mid
 						sbus_throttle = self.sbus_throttle_mid
-						# time.sleep(2)
-						target_wp += 1
+						self.sbus_cmd.data = [int(sbus_steering), int(sbus_throttle)]
+						self.sbus_cmd_pub.publish(self.sbus_cmd)
+						print("Delay as {:.2f} seconds".format(self.delay_target_list[self.target_wp]))
+						time.sleep(self.delay_target_list[self.target_wp])
+						self.target_wp += 1
 
 					######################################################################
 					##### Step 6 check every time that target_wp is completed or not #####
 					######################################################################
-					if target_wp == len(self.lat_target_list):
-						target_wp = 0
+					if self.target_wp == len(self.lat_target_list):
+						self.target_wp = 0
 						# self.ROBOT_MODE = "MANUAL"
 						self.completed_flag = True
 						sbus_steering = self.sbus_steering_mid
@@ -654,7 +798,7 @@ class GPS_NAV:
 
 					if (step != 4) and (not cart_mode_check_lock):
 						print("step: {:d} | target_wp: {:d} | goal_hdg: {:.3f} | hdg: {:.3f} | goal_dist: {:.3f} | cur_dist: {:.3f}".format(\
-							step, target_wp, goal_hdg, self.hdg, goal_dist, cur_dist))
+							step, self.target_wp, goal_hdg, self.hdg, goal_dist, cur_dist))
 
 					pause_flag = True
 
@@ -687,6 +831,13 @@ class GPS_NAV:
 
 					cart_mode_check_lock = True
 
+					if (self.got_new_wps):
+						print("Reset lat/lon mission during mission")
+						self.get_target_points()
+						self.target_wp = 0
+						self.got_new_wps = False
+
+
 				#########################################
 				### This is already completed mission ###
 				#########################################
@@ -701,14 +852,79 @@ class GPS_NAV:
 					self.completed_flag = False
 					pause_flag = False
 
+					if (self.got_new_wps):
+						print("Completed previous mission, load new mission")
+						self.get_target_points()
+						self.target_wp = 0
+						self.got_new_wps = False
+
+				#########################################################################
+				### Firstly start, maybe in manual/hold mode, completed_flag is False ###
+				#########################################################################
 				else:
+
+					if (self.got_new_wps):
+						print("Reset lat/lon mission before start")
+
+						self.get_target_points()
+						self.target_wp = 0
+						self.got_new_wps = False
+
 					self.sbus_cmd.data = [self.sbus_steering_mid, self.sbus_throttle_mid]
 
 
 				self.sbus_cmd_pub.publish(self.sbus_cmd)
 
+			##########################
+			### heartbeat checking ###
+			##########################
+			self.hb_period = (time.time() - self.hb_timestamp)
+			if self.use_heartbeat and (self.hb_period > self.hb_timeout):
+				if (int(self.hb_period)%self.hb_timeout == 0) or hb_trig_once:
+					print("Hearbeat timeout.. switch cart mode to hold")
+					self.atcart_mode_cmd_msg.data = 0
+					self.atcart_mode_cmd_pub.publish(self.atcart_mode_cmd_msg)
+					hb_trig_once = False
+
+			#############################################################
+			### continuously send arrived wp                          ###
+			### target_wp starts from 0, so we can send this directly ###
+			#############################################################
+			self.arrived_wp_msg.data = self.target_wp
+			self.arrived_wp_pub.publish(self.arrived_wp_msg)
+
 			rate.sleep()
 
 if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(description='GPS Waypoints Server node')
+	parser.add_argument('--param_file',
+						help="A file path of GpsWaypoints.yaml, default is the one in jmoab_autopilot_ros/cfg/")
+	parser.add_argument('--mission_file', 
+						help="A file path of mission.txt, default is the one in jmoab_autopilot_ros/waypoints/")
+
+	args = parser.parse_args(rospy.myargv()[1:])
+	print(args.param_file)
+	param_file = args.param_file
+	mission_file = args.mission_file
+
+	if param_file is None:
+		print("Use jmoab_autopilot_ros/cfg/GpsWaypoints.yaml")
+		rospack = rospkg.RosPack()
+		jmoab_autopilot_ros_path = rospack.get_path("jmoab_autopilot_ros")
+		yaml_name = "GpsWaypoints.yaml"
+		yaml_path = os.path.join(jmoab_autopilot_ros_path, "cfg", yaml_name)
+	else:
+		yaml_path = param_file
+
+
+	if mission_file is None:
+		print("Use jmoab_autopilot_ros/waypoints/mission.txt")
+		rospack = rospkg.RosPack()
+		jmoab_autopilot_ros_path = rospack.get_path("jmoab_autopilot_ros")
+		mission_name = "mission.txt"
+		mission_path = os.path.join(jmoab_autopilot_ros_path, "waypoints", mission_name)
+	else:
+		mission_path = mission_file
 	
-	a = GPS_NAV()
+	a = GPS_NAV(yaml_path, mission_path)
--- nodes/gps_waypoints_server.py
+++ nodes/gps_waypoints_server.py
@@ -7,6 +7,8 @@ from dynamic_reconfigure.server import Server
 from jmoab_autopilot_ros.cfg import GpsWaypointsConfig
 import os
 import yaml
+import argparse
+import sys
 
 def callback(config, level):
 	# rospy.loginfo("""Reconfigure Request: {int_param}, {double_param},{str_param}, {bool_param}""".format(**config))
@@ -15,42 +17,106 @@ def callback(config, level):
 	# print(config)
 	return config
 
+# def get_args():
+# 	""" Get arguments for individual tb3 deployment. """
+# 	parser = argparse.ArgumentParser(
+# 		description="TB3 Gesture Controller Launch File."
+# 	)
+
+# 	# Required arguments
+# 	parser.add_argument("-n", "--number",
+# 						action="store",
+# 						type=int,
+# 						required=False,
+# 						help="Add TB3 node namespace number.",
+# 						default=0)
+
+# 	return parser.parse_args(sys.argv[4:])
+
 if __name__ == "__main__":
+
 	rospy.init_node("gps_waypoints_server_node", anonymous = False)
 	rospy.loginfo("Start gps_waypoints_server_node")
 
 	server = Server(GpsWaypointsConfig, callback)
-
 	sv_node = "gps_waypoints_server_node"
-	rospack = rospkg.RosPack()
-	jmoab_autopilot_ros_path = rospack.get_path("jmoab_autopilot_ros")
-	yaml_name = "GpsWaypoints.yaml"
-	yaml_path = os.path.join(jmoab_autopilot_ros_path, "cfg", yaml_name)
 
+	parser = argparse.ArgumentParser(description='GPS Waypoints Server node')
+	parser.add_argument('--param_file',
+						help="A file path of GpsWaypoints.yaml, default is the one in cfg/")
+
+	#args = parser.parse_args()
+	args = parser.parse_args(rospy.myargv()[1:])	# to make it work on launch file
+	param_file = args.param_file
+
+	if param_file is None:
+		print("Use jmoab_autopilot_ros/cfg/GpsWaypoints.yaml")
+		rospack = rospkg.RosPack()
+		jmoab_autopilot_ros_path = rospack.get_path("jmoab_autopilot_ros")
+		yaml_name = "GpsWaypoints.yaml"
+		yaml_path = os.path.join(jmoab_autopilot_ros_path, "cfg", yaml_name)
+	else:
+		yaml_path = param_file
+		
+	
 	# https://answers.ros.org/question/169866/load-yaml-with-code/
 	# load yaml file to rosparam server without running server on python
 	f = open(yaml_path, 'r')
 	yamlfile = yaml.load(f)
 	rosparam.upload_params("/", yamlfile)
 
-	## get parameter from rosparam server that we just loaded above
-	# max_start_str = rosparam.get_param(sv_node+"/max_start_str")
-	# min_start_str = rosparam.get_param(sv_node+"/min_start_str")
-	# str_adj = rosparam.get_param(sv_node+"/str_adj")
-	# skid_adj = rosparam.get_param(sv_node+"/skid_adj")
-	# max_start_thr = rosparam.get_param(sv_node+"/max_start_thr")
-	# thr_adj = rosparam.get_param(sv_node+"/thr_adj")
-	# goal_dist_thresh = rosparam.get_param(sv_node+"/goal_dist_thresh")
-	# goal_ang_thresh = rosparam.get_param(sv_node+"/goal_ang_thresh")
-	# x_track_error_start = rosparam.get_param(sv_node+"/x_track_error_start")
-	# pid_hdg_out_thresh = rosparam.get_param(sv_node+"/pid_hdg_out_thresh")
-	# pid_x_out_thresh = rosparam.get_param(sv_node+"/pid_x_out_thresh")
-	# hdg_p = rosparam.get_param(sv_node+"/hdg_p")
-	# hdg_i = rosparam.get_param(sv_node+"/hdg_i")
-	# hdg_d = rosparam.get_param(sv_node+"/hdg_d")
-	# cross_p = rosparam.get_param(sv_node+"/cross_p")
-	# cross_i = rosparam.get_param(sv_node+"/cross_i")
-	# cross_d = rosparam.get_param(sv_node+"/cross_d")
+	# get parameter from rosparam server that we just loaded above
+	max_start_str = rosparam.get_param(sv_node+"/max_start_str")
+	min_start_str = rosparam.get_param(sv_node+"/min_start_str")
+	str_adj = rosparam.get_param(sv_node+"/str_adj")
+	skid_adj = rosparam.get_param(sv_node+"/skid_adj")
+	str_mid = rosparam.get_param(sv_node+"/str_mid")
+
+	max_start_thr = rosparam.get_param(sv_node+"/max_start_thr")
+	thr_adj = rosparam.get_param(sv_node+"/thr_adj")
+	thr_slowest = rosparam.get_param(sv_node+"/thr_slowest")
+	thr_mid = rosparam.get_param(sv_node+"/thr_mid")
+	
+	goal_dist_thresh = rosparam.get_param(sv_node+"/goal_dist_thresh")
+	goal_ang_thresh = rosparam.get_param(sv_node+"/goal_ang_thresh")
+	x_track_error_start = rosparam.get_param(sv_node+"/x_track_error_start")
+	x_track_repose_dist = rosparam.get_param(sv_node+"/x_track_repose_dist")
+
+	pid_hdg_out_thresh = rosparam.get_param(sv_node+"/pid_hdg_out_thresh")
+	pid_x_out_thresh = rosparam.get_param(sv_node+"/pid_x_out_thresh")
+
+	hdg_p = rosparam.get_param(sv_node+"/hdg_p")
+	hdg_i = rosparam.get_param(sv_node+"/hdg_i")
+	hdg_d = rosparam.get_param(sv_node+"/hdg_d")
+	cross_p = rosparam.get_param(sv_node+"/cross_p")
+	cross_i = rosparam.get_param(sv_node+"/cross_i")
+	cross_d = rosparam.get_param(sv_node+"/cross_d")
+
+	use_heartbeat = rosparam.get_param(sv_node+"/use_heartbeat")
+
+	rospy.loginfo("Load these parameters to server")
+	rospy.loginfo("max_start_str : {:}".format(max_start_str))
+	rospy.loginfo("min_start_str : {:}".format(min_start_str))
+	rospy.loginfo("str_adj : {:}".format(str_adj))
+	rospy.loginfo("skid_adj : {:}".format(skid_adj))
+	rospy.loginfo("str_mid : {:}".format(str_mid))
+	rospy.loginfo("max_start_thr : {:}".format(max_start_thr))
+	rospy.loginfo("thr_adj : {:}".format(thr_adj))
+	rospy.loginfo("thr_slowest : {:}".format(thr_slowest))
+	rospy.loginfo("thr_mid : {:}".format(thr_mid))
+	rospy.loginfo("goal_dist_thresh : {:}".format(goal_dist_thresh))
+	rospy.loginfo("goal_ang_thresh : {:}".format(goal_ang_thresh))
+	rospy.loginfo("x_track_error_start : {:}".format(x_track_error_start))
+	rospy.loginfo("x_track_repose_dist : {:}".format(x_track_repose_dist))
+	rospy.loginfo("pid_hdg_out_thresh : {:}".format(pid_hdg_out_thresh))
+	rospy.loginfo("pid_x_out_thresh : {:}".format(pid_x_out_thresh))
+	rospy.loginfo("hdg_p : {:}".format(hdg_p))
+	rospy.loginfo("hdg_i : {:}".format(hdg_i))
+	rospy.loginfo("hdg_d : {:}".format(hdg_d))
+	rospy.loginfo("cross_p : {:}".format(cross_p))
+	rospy.loginfo("cross_i : {:}".format(cross_i))
+	rospy.loginfo("cross_d : {:}".format(cross_d))
+	rospy.loginfo("use_heartbeat : {:}".format(use_heartbeat))
 
 	# server.update_configuration({"max_start_str":1094})
 	rospy.spin()
